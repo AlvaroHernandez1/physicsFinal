e4fbefb0d9191b431c90753c4e9ac349d36822a7
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2026-05-22 11:01:47 -0400

    Added repeating background image

--- angMom.py
+++ angMom.py
@@ -1,1 +1,18 @@
 from vpython import *
+scene = canvas(width=600, height=600, background=color.white)
+scene.ambient = color.black
+
+scene.userspin = False
+scene.userpan = False
+tiles = 4
+tile = 0
+y=100
+x=100
+while (tile < tiles):
+    y_temp = y
+    y=x * -1
+    x = y_temp
+    box (pos=vector(x,y,0), size=vector(200,200,200), texture="ice_tile.png")
+    tile+=1
+
+#sphere(pos=vec(0,0,0), texture=textures.wood, color=color.cyan)
