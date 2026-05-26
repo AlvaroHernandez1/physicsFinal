ec7f2b873b62898d377d9c09ebb83dee63fb21f5
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2026-05-26 09:45:49 -0400

    fixed background

--- angMom.py
+++ angMom.py
@@ -1,9 +1,11 @@
 from vpython import *
 scene = canvas(width=600, height=600, background=color.white)
-scene.ambient = color.black
+scene.ambient = color.white
+scene.lights = []
+scene.userspin = True
+scene.userpan = True
+scene.range = 100
 
-scene.userspin = False
-scene.userpan = False
 tiles = 4
 tile = 0
 y=100
@@ -12,7 +14,7 @@ while (tile < tiles):
     y_temp = y
     y=x * -1
     x = y_temp
-    box (pos=vector(x,y,0), size=vector(200,200,200), texture="ice_tile.png")
+    box (pos=vector(x,y,0), size=vector(200,200,0.1), texture="ice_tile.png")
     tile+=1
 
 #sphere(pos=vec(0,0,0), texture=textures.wood, color=color.cyan)
