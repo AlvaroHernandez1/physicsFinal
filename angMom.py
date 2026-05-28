71486ef733fc7c4e619bc40b70896177ad769a99
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2026-05-28 09:48:30 -0400

    Added the two classes

--- angMom.py
+++ angMom.py
@@ -1,11 +1,30 @@
 from vpython import *
+
+class Skater
+    def __init__(position, velocity, mass, collision_position):
+        self.position = position
+        self.velocity = velocity
+        self.mass = mass
+        self.collision_position = collision_position
+
+     
+
+class Pole
+    def __init__(mass, length):
+        self.mass = mass
+        self.length = length
+        self.I = mass * length * length * (1/12)
+
+        self.velocity = 0
+
+
 scene = canvas(width=600, height=600, background=color.white)
 scene.ambient = color.white
 scene.lights = []
 scene.userspin = True
 scene.userpan = True
 scene.range = 100
-
+ice_texture = "https://i.imgur.com/SCIkDjk.png"
 tiles = 4
 tile = 0
 y=100
@@ -14,7 +33,7 @@ while (tile < tiles):
     y_temp = y
     y=x * -1
     x = y_temp
-    box (pos=vector(x,y,0), size=vector(200,200,0.1), texture="ice_tile.png")
+    box (pos=vector(x,y,0), size=vector(200,200,0.1), texture=ice_texture)
     tile+=1
 
-#sphere(pos=vec(0,0,0), texture=textures.wood, color=color.cyan)
+
