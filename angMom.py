12163f9453aeaf0932c659321ea81e2d1d1d3d7a
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2026-05-29 09:58:05 -0400

    Added a pole

--- angMom.py
+++ angMom.py
@@ -7,6 +7,7 @@ class Skater:
         self.mass = mass
         self.collision_position = collision_position
         self.ball = sphere(pos=self.position, radius = 15, color = color.cyan)
+        self.L = 0
 
     def updatePosition(self, time):
         self.position += self.velocity * time
@@ -14,7 +15,8 @@ class Skater:
 
     def updateL(self, cor):
         r = self.position - cor
-        L = mag(cross(r, self.velocity))
+        self.L = cross(r, self.velocity)
+        
 
 
 
@@ -23,12 +25,10 @@ class Pole:
         self.mass = mass
         self.length = length
         self.I = mass * length * length * (1/12)
+        self.velocity = vector(0,0,0)
+        self.position = vector(0,0,0)
+        self.body = box(pos=self.position, length = self.length, width = 1, height = 10, color = color.cyan)
 
-        self.velocity = 0
-
-def reset_simulation():
-    isRunning = False
-    scene.camera.pos = vector(0,0,0)
 
 scene = canvas(width=600, height=600, background=color.white)
 scene.ambient = color.white
@@ -36,13 +36,23 @@ scene.lights = []
 scene.userspin = False
 scene.userpan = False
 scene.range = 100
-button(bind=reset_simulation, text="Reset Simulation")
 isRunning = True
 skaterList = []
 skaterList.append(Skater(vector(-25,100,0), vector(0, -50, 0), 10, 5))
 skaterList.append(Skater(vector(25,-100,0), vector(0, 50, 0), 10, 5))
+pole = Pole(10, 92)
 
 lastTime = 0
+
+def reset_simulation():
+    isRunning = False
+    scene.camera.pos = vector(0,0,0)
+    for skater in skaterList:
+        skater.ball.visible = False
+        del skater
+    isRunning = True
+
+button(bind=reset_simulation, text="Reset Simulation")
 
 ice_texture = "https://i.imgur.com/SCIkDjk.png"
 tiles = 4
@@ -61,5 +71,6 @@ while isRunning:
     rate(60)
     for skater in skaterList:
         skater.updatePosition(1/60.0)
+    
 
 
